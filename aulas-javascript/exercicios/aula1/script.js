99538629c79dbcd2efc57f43c33432f26987476d
debora zanoello <deborazanoello@gmail.com>
2020-10-06 23:19:24 -0300

    Exercícios de Javascript 1 e 2 Débora Zanoello

---
+++ aulas-javascript/exercicios/aula1/script.js
@@ -0,0 +1,13 @@
+function checarSePodeDirirgir (idade) {
+
+if (idade >= 18) {
+    alert ('Já pode dirigir!');
+} else {
+    alert ('Ainda não pode dirigir!');
+
+}
+}
+let idade = prompt ('Digite sua idade');
+checarSePodeDirirgir (idade); 
+
+
---
+++ aulas-javascript/exercicios/aula2/script.js
@@ -0,0 +1,23 @@
+function saoDiferentes() {
+    let input1 = document.querySelector('#input1').valueAsNumber;
+    let input2 = document.querySelector('#input2').valueAsNumber;
+
+if(input1==input2)
+{
+    let div = document.createElement('div');
+    div.innerText = 'Os valores são diferentes' ;
+    document.body.append(div);
+    div.style.color = 'green';
+}
+else {
+    let div = document.createElement('div');
+    div.innerText = 'Não são diferentes';
+    document.body.append(div);
+    div.style.color = 'red';
+}
+saoDiferentes()
+}
+function resetar() {
+    document.getElementById('input1').remove();
+    document.getElementById('input2').remove();
+}
